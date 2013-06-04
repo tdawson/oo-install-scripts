c54375a8c390ce6a78f094752feefece5c34c170
Troy Dawson <tdawson@redhat.com>
2013-06-04 09:13:49 -0500

    fixing up broker-mongodb to be more automated

--- broker-mongodb.sh
+++ broker-mongodb.sh
@@ -5,13 +5,14 @@
 yum -y install mongodb-server
 
 # Tweek config file
-echo ""
-echo "Tweak mongodb config file"
-echo "  Uncomment auth = true"
-echo "  Add smallfiles = true"
-echo "  Hit return to start"
-read tempkey
-vi /etc/mongodb.conf
+#echo ""
+#echo "Tweak mongodb config file"
+#echo "  Uncomment auth = true"
+#echo "  Add smallfiles = true"
+#echo "  Hit return to start"
+#read tempkey
+#vi /etc/mongodb.conf
+sed -i -e 's|#auth = true|auth = true\nsmallfiles = true|g' /etc/mongodb.conf
 
 # Setup and start service
 /usr/bin/systemctl enable mongod.service
@@ -24,5 +25,7 @@ echo "Testing that mongodb works"
 echo "  type in the following"
 echo "    show dbs"
 echo "    exit"
+echo "  Hit return to start"
+read tempkey
 mongo
 

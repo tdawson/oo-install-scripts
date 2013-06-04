27cc82bbe56556ba41779d8d94a720da32c07063
Troy Dawson <tdawson@redhat.com>
2013-06-04 10:33:23 -0500

    fixing broker-node-auth

--- broker-node-auth-setup.sh
+++ broker-node-auth-setup.sh
@@ -5,14 +5,14 @@
 source ./oo-install.conf
 
 # Put node information in DNS
-oo-register-dns -h node -d ${DOMAIN} -n ${NODEIP} -k ${KEYFILE}
+oo-register-dns -h ${NODENAME} -d ${DOMAIN} -n ${NODEIP} -k ${KEYFILE}
 
 # Copy over broker pub key info
-scp /etc/openshift/rsync_id_rsa.pub root@${NODENAME}:/root/.ssh/
+scp /etc/openshift/rsync_id_rsa.pub root@${NODEHOSTNAME}:/root/.ssh/
 
 # Setup broker pub key for authentication
-ssh root@${NODENAME} "cat /root/.ssh/rsync_id_rsa.pub >> /root/.ssh/authorized_keys ; rm -f /root/.ssh/rsync_id_rsa.pub"
+ssh root@${NODEHOSTNAME} "cat /root/.ssh/rsync_id_rsa.pub >> /root/.ssh/authorized_keys ; rm -f /root/.ssh/rsync_id_rsa.pub"
 
 # Verify you can ssh in without password
-ssh root@${NODENAME} "hostname ; uptime"
+ssh root@${NODEHOSTNAME} "hostname ; uptime"
 

7ce34d0f2c0da545e03be77b947e650d6e9d4bba
Troy Dawson <tdawson@redhat.com>
2013-06-05 11:41:12 -0500

    removed final manual setting

--- node-pam-cgroups-quota.sh
+++ node-pam-cgroups-quota.sh
@@ -26,17 +26,22 @@ done
 /usr/sbin/service openshift-cgroups restart
 
 # DISK QUOTA
-echo ""
-echo "Add usrquota to filesystem"
-echo "  add usrquota to whichever filesystem has /var/lib/openshift on it"
-echo "    Example:"
-echo "      /dev/sda1 / ext4    defaults,usrquota 1 1"
-echo ""
-echo "  Hit enter to edit fstab with vi"
-read tempkey
-vi /etc/fstab
+#echo ""
+#echo "Add usrquota to filesystem"
+#echo "  add usrquota to whichever filesystem has /var/lib/openshift on it"
+#echo "    Example:"
+#echo "      /dev/sda1 / ext4    defaults,usrquota 1 1"
+#echo ""
+#echo "  Hit enter to begin editing fstab with vi"
+#read tempkey
+#vi /etc/fstab
+/bin/cp -f /etc/fstab /etc/fstab.save.openshift
+FS="/var/lib | grep -v Filesystem | awk '{print $6}'"
+sed -i "s|^\S*\s\+${FS}\s\+\S*\s\+|&usrquota,|" /etc/fstab
 
 # reboot or remount
 mount -o remount /
+
+# test
 quotacheck -cmug /
 

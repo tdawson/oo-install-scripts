18c1cc2a629ab52ad6cff3eaf9953e0935cc2e84
Troy Dawson <tdawson@redhat.com>
2013-07-27 18:15:48 -0500

    update for rhel6

--- common-date-time.sh
+++ common-date-time.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # avoid clock skew
+# setup variables
+source ./oo-install.conf
+
 yum -y install ntp
-/bin/systemctl enable ntpd.service
-/bin/systemctl start  ntpd.service
+
+if [ "$DISTRO" == "rhel6" ] ; then
+    chkconfig ntpd on
+    service ntpd start
+else
+    systemctl enable ntpd.service
+    systemctl start  ntpd.service
+fi
+
+
